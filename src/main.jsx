5277d01e5fc3be2858de85dd5b7c48365490b7b2
carlo120686 <105112940+carlo120686@users.noreply.github.com>
2026-03-09 22:59:44 +0100

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-createRoot(document.getElementById('root')).render(<StrictMode><App /></StrictMode>)
+import { StoreProvider } from './store/index.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <StoreProvider>
+      <App />
+    </StoreProvider>
+  </React.StrictMode>
+)
